ecc88ed79609b9ae379e996f9e565760a852f4c6
cga <carolinagalmeida.eng@gmail.com>
2021-05-10 13:17:23 -0300

    pratica-1

---
+++ dom/pratica-01/app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Coder',
+        idade: 28,
+        imagem: "https://image.freepik.com/vetores-gratis/os-programadores-de-desenvolvimento-de-software-investigam-o-codigo_90220-174.jpg"
+    },
+    
+})
